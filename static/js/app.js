aaf2f5f9092ebd9d76f7cbf506f1d2679d30b4d7
harshadvali11 <harshadvali1431@gmail.com>
2022-12-07 18:55:35 +0530

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+function click1() {
+    let text1=document.querySelector('#content1');
+    let cardBody1=document.querySelector('#card1');
+    let Bike1=document.querySelector('#Bike1');
+    Bike1.innerText="About Himalayan"
+    text1.innerText="It is a machine that one can take to the Himalayas or to the corner grocery store with equal ease."
+    cardBody1.style.backgroundColor="Grey";
+    cardBody1.style.color="white";
+    text1.style.padding="10px"
+    text1.style.color="white";
+    text1.style.textAlign="justify"
+    text1.style.boxShadow="0 0 15px white"
+}
+function click2() {
+    let text2=document.querySelector('#content2');
+    let cardBody2=document.querySelector('#card2');
+    let Bike2=document.querySelector('#Bike2');
+    Bike2.innerText="About Triumph"
+    text2.innerText="Triumph Bonneville bikes are known for their neutral, easy-handling, and delightful riding experience."
+    cardBody2.style.backgroundColor="Olive";
+    cardBody2.style.color="Black";
+    text2.style.padding="10px"
+    text2.style.color="Black";
+    text2.style.textAlign="justify"
+    text2.style.boxShadow="0 0 15px white"
+
+}
+function click3() {
+    let text3=document.querySelector('#content3');
+    let cardBody2=document.querySelector('#card3');
+    let Bike3=document.querySelector('#Bike3');
+    Bike3.innerText="About Honda H-Ness";
+    text3.style.textShadow="0 0 15px black"
+    text3.innerText="A majestic motorcycle which is a perfect blend of advanced new world features and the classic old-world charm."
+    cardBody2.style.backgroundColor="Teal";
+    cardBody2.style.color="white";
+    text3.style.padding="10px"
+    text3.style.color="white";
+    text3.style.textAlign="justify"
+    text3.style.boxShadow="0 0 15px white"
+
+}
